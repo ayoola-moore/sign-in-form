044e1985c9b9b2919ee92f0d4c921e0c99a33fd3
hayoola <onaolapo.ayo@gmail.com>
2021-01-18 09:52:55 +0400

    Updates

---
+++ precache-manifest.b5211fa46e90e3a827e953cbe5554e83.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbf8e037aee8aefc6c9a8419ee956a29",
+    "url": "/sign-in-form/index.html"
+  },
+  {
+    "revision": "9d034ba8d851ee898499",
+    "url": "/sign-in-form/static/css/main.00a5ec12.chunk.css"
+  },
+  {
+    "revision": "204dbb1833527967c247",
+    "url": "/sign-in-form/static/js/2.56ee01d2.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/sign-in-form/static/js/2.56ee01d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d034ba8d851ee898499",
+    "url": "/sign-in-form/static/js/main.cb8b60f6.chunk.js"
+  },
+  {
+    "revision": "47c1cbce27d7cc32aa4f",
+    "url": "/sign-in-form/static/js/runtime-main.b0516b5d.js"
+  }
+]);
